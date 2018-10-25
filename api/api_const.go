1a4c134d8daf3a6063d6adcd2441ca47ac390549
Evan <devillordking@gmail.com>
2018-10-25 16:50:27 +1100

    initial commit

---
+++ api/api_const.go
@@ -0,0 +1,4 @@
+package api
+
+const Api_Url = "https://api.appstoreconnect.apple.com"
+const Api_version = "v1"
---
+++ main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+  "fmt"
+  "github.com/eexe1/propeller/api"
+)
+
+func main()  {
+  fmt.Println("test")
+  _ = api.Api_version
+}
